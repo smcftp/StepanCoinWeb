4eec321e7e5384f66e0bd585d3bce71438a89096
smcftp <156796937+smcftp@users.noreply.github.com>
2025-04-18 19:42:28 +0300

    Update Stickers.tsx

--- src/components/Stickers.tsx
+++ src/components/Stickers.tsx
@@ -8,9 +8,9 @@ export default function Stickers() {
              absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-6 xs:px-[116px]  w-full">
             Наш <span className="text-sulu">стикерпак</span> уже доступен в Телеграме!
             </h2>
-            <Image src='/sc_st_2.svg' alt="Sticker 1" width={140} height={140} className="absolute top-4 left-4 xs:scale-125 sm:scale-[2] sm:top-14 sm:left-20"/>
+            <Image src='/sc_st_6.png' alt="Sticker 1" width={140} height={140} className="absolute top-4 left-4 xs:scale-125 sm:scale-[2] sm:top-14 sm:left-20"/>
             <Image src='/sc_st_5.png' alt="Sticker 2" width={140} height={140} className="absolute top-4 right-4 xs:scale-125 sm:scale-[2] sm:top-14 sm:right-20"/>
-            <Image src='/sc_st_6.png' alt="Sticker 3" width={140} height={140} className="absolute bottom-4 right-4 xs:scale-125 sm:scale-[2] sm:bottom-14 sm:right-20"/>
+            <Image src='/sc_st_2.svg' alt="Sticker 3" width={140} height={140} className="absolute bottom-4 right-4 xs:scale-125 sm:scale-[2] sm:bottom-14 sm:right-20"/>
             <Image src='/sc_st_4.png' alt="Sticker 4" width={140} height={140} className="absolute bottom-4 left-4 xs:scale-125 sm:scale-[2] sm:bottom-14 sm:left-20"/>
         </section>
     )
